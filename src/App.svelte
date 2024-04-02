8920331a92167aff8ee8ba239bee5e10233616ba
csslick <digitone@naver.com>
2024-04-02 16:07:02 +0900

    5.반복문 each

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
   import logo1 from './assets/svelte.svg';
   let logo2 = '/vite.svg';
-  let title = "영화제목 1";
-  let year = 2024;
-  let category = "액션";
   let textRed = "color: red;"
 
   const data = [
+    {
+      title: "파묘",
+      year: 2024,
+      category: "미스터리, 공포",
+    },
     {
       title: "고질라 X 콩: 뉴 엠파이어",
       year: 2024,
@@ -18,23 +20,25 @@
       category: "액션",
     },
   ];
+
+  const foods = ['pizza', 'burger', 'sushi', 'pasta'];
 </script>
 
 <main>
+  {#each foods as food, i}
+    <p>{i}: {food}</p>
+  {/each}
   <h1>영화정보</h1>
   <img src={logo1} alt="">
   <img src={logo2} alt="">
-  <div>
-    <h3 class="bg-yellow" style={textRed}>{data[0].title}</h3>
-    <p>개봉: {data[0].year}</p>
-    <p>장르: {data[0].category}</p>
-    <p>{"hello".toUpperCase()}</p>
-  </div>
-  <div>
-    <h3 class="bg-yellow" style={textRed}>{data[1].title}</h3>
-    <p>개봉: {data[1].year}</p>
-    <p>장르: {data[1].category}</p>
-  </div>
+  {#each data as movie, i}
+    <div>
+      <h3 class="bg-yellow" style={textRed}>{movie.title}</h3>
+      <h3>{movie.title}</h3>
+      <p>개봉: {movie.year}</p>
+      <p>장르: {movie.category}</p>
+    </div>
+  {/each}
 </main>
 
 <style>
